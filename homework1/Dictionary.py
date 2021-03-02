8284b6b95ff9f025b2f4c11e89b764311a4d04f3
Jonathan Tang <27tangj@D103.org>
2021-03-02 00:58:09 +0000

    contains_word

---
+++ homework1/Dictionary.py
@@ -0,0 +1,14 @@
+from homework1.Letter import Letter
+
+class Dictionary:
+  def __init__(self):
+    self.start_letter = Letter(False, {})
+  
+
+  # imagine adding the word 'cat'
+  def add_word(word):
+    pass
+  
+  def contains_word(word):
+    # this code has not stared yet.
+    pass
---
+++ homework1/Letter.py
@@ -0,0 +1,6 @@
+class Letter:
+  # is_word = 
+  # next_letters = 
+  def __init__(self, is_word, next_letters):
+    self.is_word = is_word
+    self.next_letters = next_letters
